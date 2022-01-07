325848bdd1ee0c1f6d313724fde2cc31f7a9cfa6
MEJRI ISSAM <34856078+imejri@users.noreply.github.com>
2022-01-07 14:36:21 +0100

    Update helloWorldSimple.groovy

--- vars/helloWorldSimple.groovy
+++ vars/helloWorldSimple.groovy
@@ -1,8 +1,11 @@
 
-def call(String name, String dayOfWeek) {
-  sh "echo Hello World ${name}. It is ${dayOfWeek}."
+//def call(String name, String dayOfWeek) {
+  def call() {
+  //sh "echo Hello World ${name}. It is ${dayOfWeek}."
   def BUILD_FOLDER='/infra/build/'
+  sh "echo the biuild directory is ${BUILD_FOLDER}"
   sh "echo ${env.WORKSPACE}"
+  sh "echo the build run in ${NODE_NAME}"
   echo "*****************"
   sh "echo ${BUILD_FOLDER}"
   echo "*****************"
